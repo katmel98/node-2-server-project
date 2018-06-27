a8e9a1acf85f166d5d15c52bf39d7c084bc287dd
Melvin Sequera <katmel98@gmail.com>
2018-06-27 12:41:22 +0200

    Created page Projects

--- server.js
+++ server.js
@@ -56,6 +56,13 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+    });
+});
+
+
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
